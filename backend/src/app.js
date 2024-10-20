52662674801defde85181b57ad8c0399ad0f9e80
musamusakannike <musamusakannike@gmail.com>
2024-10-20 19:26:06 +0100

    Dishful server and client created

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const connectDB = require('./config/db.config');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+
+const authRoutes = require("./routes/auth.route")
+
+connectDB()
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'SERVER IS RUNNING...',
+  });
+});
+
+app.use("api/v1/auth", authRoutes);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const registerUser = async (req, res) => {};
+
+const loginUser = async (req, res) => {};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
