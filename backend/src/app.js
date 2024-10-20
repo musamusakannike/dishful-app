1d7e984aa8c96bfbf98e90aa1a37555bc443dc9c
musamusakannike <musamusakannike@gmail.com>
2024-10-20 19:48:12 +0100

    Updated server auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use("api/v1/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -21,7 +21,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { userId: user._id, username: user.username },
     process.env.JWT_SECRET,
-    { expiresIn: "1h" }
+    { expiresIn: "72h" }
   );
 };
 
