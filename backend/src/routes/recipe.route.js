89938719e15bb42d3a8ee87b2abd15c2b4b63903
musamusakannike <musamusakannike@gmail.com>
2024-10-27 08:58:29 +0100

    added leftovers route

--- backend/src/routes/recipe.route.js
+++ backend/src/routes/recipe.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authenticate = require("../middlewares/auth.middleware")
-const {genTextRecipe, genIngredientsRecipe, genRandomRecipe} = require("../controllers/recipe.controller")
+const {genTextRecipe, genIngredientsRecipe, genRandomRecipe, genLeftoversRecipe} = require("../controllers/recipe.controller")
 
 const router = express.Router();
 
 router.post("/text-recipe", authenticate, genTextRecipe);
 router.post("/ingredients-recipe", authenticate, genIngredientsRecipe);
 router.post("/random-recipe", authenticate, genRandomRecipe);
+router.post("/leftovers-recipe", authenticate, genLeftoversRecipe);
 
 module.exports = router;
