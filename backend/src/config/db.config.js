e31d5b12b3bc80b3863cc2318d7f70265145023f
musamusakannike <musamusakannike@gmail.com>
2024-10-27 20:48:57 +0100

    edited connection to mongodb

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('Connected to database');
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+  const clientOptions = {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+  };
+  try {
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
